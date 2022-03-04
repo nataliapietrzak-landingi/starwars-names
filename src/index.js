8d7fa48b918f684cf8b0655b26541880a16638e9
Natalia <natalia.pietrzak@landingi.com>
2022-03-04 11:25:27 +0100

    Change name

--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
-// let uniqueRandomArray
-
-// (async function(){
-//   var lib = await import("unique-random-array")
-//   uniqueRandomArray = lib.default()
-//   console.log(uniqueRandomArray(['a', 'b']))
-// })()
-
 var uniqueRandomArray = require('unique-random-array')
 
 var starWarsNames = require('./starwars-names.json')
